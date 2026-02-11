274e92ebcbc665cb9dcba1a42c56d2c8c152ce68
c24091120 <Yousefg@cardiff.ac.uk>
2026-02-11 11:22:50 +0000

    added (10) most popular ai domains to be scanned for

---
+++ scripts/constants.js
@@ -0,0 +1,12 @@
+export const POPULAR_AI_DOMAINS = [
+  "gemini.google.com", 
+  "copilot.microsoft.com",
+  "deepseek.com", 
+  "chatgpt.com", 
+  "openai.com", 
+  "claude.ai", 
+  "perplexity.ai", 
+  "mistral.ai", 
+  "grok.com",
+  "x.ai"
+];
